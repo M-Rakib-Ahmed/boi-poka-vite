59820389fd2577009622ce50f8cd9e66d2968161
M Rakib Ahmed <ahmedmrakib24@gmail.com>
2024-12-17 17:38:51 +0600

    new file added

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,7 +1,7 @@
 
 
 const Book = ({book}) => {
-    const {image, bookName, author, tags} = book
+    const {image, bookName, author, tags,category} = book
     return (
         <div className="card bg-base-100 w-96 shadow-xl p-6">
   <figure className="bg-[#F3F3F3] py-8 rounded-xl">
@@ -17,12 +17,19 @@ const Book = ({book}) => {
     </div>
     <h2 className="card-title">
       {bookName}
-      <div className="badge badge-secondary">NEW</div>
+      <div className="badge badge-secondary">NEWS</div>
     </h2>
     <p>By:{author}</p>
-    <div className="card-actions justify-end">
-      <div className="badge badge-outline">Fashion</div>
-      <div className="badge badge-outline">Products</div>
+    <div className="border-t-2 border-dashed"></div>
+    <div className="card-actions justify-between">
+      <div className="badge badge-outline">{category}</div>
+     <div className="rating">
+  <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
+  <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" defaultChecked />
+  <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
+  <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
+  <input type="radio" name="rating-4" className="mask mask-star-2 bg-green-500" />
+</div>
     </div>
   </div>
 </div>
